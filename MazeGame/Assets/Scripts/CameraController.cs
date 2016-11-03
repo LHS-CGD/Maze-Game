a479d8223b3a42c32835a1c2dd6ca24f20ea6638
kNelakonda <kavi.nelakonda@gmail.com>
2016-11-03 11:05:32 -0700

    small changes

--- MazeGame/Assets/Scripts/CameraController.cs
+++ MazeGame/Assets/Scripts/CameraController.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject myo = null;
-	private ThalmicMyo thalmicMyo;
+
 	private Transform playerTransform;
 
 	private Vector3 offset;
@@ -14,14 +13,10 @@ public class CameraController : MonoBehaviour {
 	void Start () {
 		playerTransform = player.transform;
 		offset = transform.position - player.transform.position;
-		thalmicMyo = myo.GetComponent<ThalmicMyo> ();
-		transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
+
+
 	}
 
-	void Update() {
-		this.transform.rotation = new Quaternion(thalmicMyo.transform.rotation.x, thalmicMyo.transform.rotation.y,
-			thalmicMyo.transform.rotation.z, thalmicMyo.transform.rotation.w);
-	}
 
 	void LateUpdate () {
 		
